87e830c3b8b6960a9f76cd1d3cd2e8c783fa84ba
Ivan Kaho Chan <ivan.kaho.chan@gmail.com>
2017-05-15 11:19:56 -0400

    Add person name to details

---
+++ index.js
@@ -0,0 +1,12 @@
+const personForm = document.querySelector('form')
+
+personForm.onsubmit = (event) => {
+    event.preventDefault()
+    const form = event.target
+    const details = document.querySelector('.details')
+
+
+    const personName = (form.personName.value)
+    details.innerHTML = '<em>' + personName + '</em>'
+
+}
